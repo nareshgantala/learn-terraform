8049c06951aee2b418562c92172f06980488ad0a
nareshgantala <nareshgantala@gmail.com>
2024-10-09 21:16:14 +0530

    for_each

--- for_each/main.tf
+++ for_each/main.tf
@@ -6,10 +6,31 @@ resource "null_resource" "fruits" {
   }
 }
 
+resource "null_resource" "fruits1" {
+    for_each = var.fruits1
+    provisioner "local-exec" {
+      command = "echo fruit name - ${each.key} - ${each.value["count"]}"
+    }
+  
+}
+
 variable "fruits" {
   default = {
     apple = 100,
     banana = 200,
     orange = 500
+  }
+}
+
+variable "fruits1" {
+  default = {
+    apple = {
+        name = apple
+        count = 150
+    }
+    banana = {
+        name = banana
+        count = 250
+    }
   }
 }
