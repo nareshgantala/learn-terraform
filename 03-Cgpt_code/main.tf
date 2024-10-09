e6f6eef636713d9a3c787375fdd2e5f72c7474a7
nareshgantala <nareshgantala@gmail.com>
2024-10-09 07:40:18 +0530

    id

--- 03-Cgpt_code/main.tf
+++ 03-Cgpt_code/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "frontend" {
   count = length(data.aws_security_group.selected) > 0 ? 1 : 0
   ami           = "ami-0b4f379183e5706b9"
   instance_type = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.selected]
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
 
   tags = {
     Name = "frontend"
